9a8a1e591ad051c5e67d86aa9e6d9da2c4102ba7
Jorge <jorgelorenzo188@gmail.com>
2023-09-20 19:19:52 +0100

    Fundamentals - operators done

--- 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
+++ 2. Flow Control/Loops/2.8 Hundredth Prime/src/HundredthPrime.java
@@ -2,6 +2,22 @@ public class HundredthPrime
 {
    public static void main(String[] args)
    {
-      /* Make a code that displays the 100th prime number */
+      int cont_number_prime = 0;
+      int prime_number = 1;
+      boolean prime_encounter;
+      while (cont_number_prime < 100) {
+         prime_encounter = true;
+         prime_number++;
+         for (int divisor = prime_number - 1; divisor > 1; divisor--) {
+            if (prime_number % divisor == 0) {
+               prime_encounter = false;
+               break;
+            }
+         }
+         if (prime_encounter == true) {
+            cont_number_prime++;
+         }
+      }
+      System.out.println(prime_number);
    }
 }
